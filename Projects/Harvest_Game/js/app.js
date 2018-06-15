a1eb9c9c5bd5b409cdce21dd01d992f64a3603d2
Joseph <josephedmed@hotmail.com>
2018-06-16 03:52:33 +0700

    Made field and grass generator

--- Projects/Harvest_Game/js/app.js
+++ Projects/Harvest_Game/js/app.js
@@ -1,3 +1,21 @@
 $(() => {
-  console.log('test');
+  const $container = $('.container')
+
+  const makeField = () => {
+    for (let i = 0; i < 96; i++) {
+      const $div = $('<div>');
+      $div.addClass('square');
+      $container.append($div);
+    }
+  }
+
+  const generateWeeds = () => {
+    for (let i = 0; i < 56; i++) {
+      $container.children().eq(Math.floor(Math.random()*$container.children().length)).addClass('weeds')
+    }
+  }
+
+
+  makeField();
+  generateWeeds();
 });
