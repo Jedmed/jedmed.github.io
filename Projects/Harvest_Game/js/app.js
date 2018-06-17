8badfde46cb3807e80b8833dfc67c88112d2cb53
Joseph <josephedmed@hotmail.com>
2018-06-17 18:53:00 +0700

    Added plant functionality

--- Projects/Harvest_Game/js/app.js
+++ Projects/Harvest_Game/js/app.js
@@ -1,5 +1,22 @@
 $(() => {
-  const $container = $('.container')
+  // Get Elements
+  const $container = $('.container');
+  let player = ['one', 'two'];
+
+
+  // Event Handlers
+  const rotatePlayer = () => {
+    tempPlayer = player.shift(1);
+    player.push(tempPlayer);
+  }
+
+  const plant = () => {
+    if (!($(event.target).hasClass('plant')) && !($(event.target).hasClass('weeds'))) {
+      $(event.target).addClass('plant');
+      $(event.target).addClass(player[0]);
+      rotatePlayer();
+    }
+  }
 
   const makeField = () => {
     for (let i = 0; i < 96; i++) {
@@ -11,11 +28,14 @@ $(() => {
 
   const generateWeeds = () => {
     for (let i = 0; i < 56; i++) {
-      $container.children().eq(Math.floor(Math.random()*$container.children().length)).addClass('weeds')
+      $container.children().eq(Math.floor(Math.random() * $container.children().length)).addClass('weeds')
     }
   }
 
 
   makeField();
   generateWeeds();
+  // Event Listeners
+  const $square = $('.square');
+  $square.on('click', plant)
 });
