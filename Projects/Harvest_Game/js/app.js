a0abe071e8ebeefaed1a6d1410122d8425de2dc5
Joseph <josephedmed@hotmail.com>
2018-06-18 10:33:34 +0700

    Added Weed removal functionality

--- Projects/Harvest_Game/js/app.js
+++ Projects/Harvest_Game/js/app.js
@@ -3,21 +3,32 @@ $(() => {
   const $container = $('.container');
   let player = ['one', 'two'];
 
-
   // Event Handlers
+  // Rotate players per action
   const rotatePlayer = () => {
     tempPlayer = player.shift(1);
     player.push(tempPlayer);
   }
 
-  const plant = () => {
+  // check if player is selecting a weed and if so remove it, rotate player
+  const removeWeeds = (event) => {
+    if ($(event.target).hasClass('weeds') === true) {
+      $(event.target).removeClass('weeds')
+      rotatePlayer();
+    }
+  }
+
+  // check if players can plant a seed, and if they can do so, rotate player
+  const plant = (event) => {
     if (!($(event.target).hasClass('plant')) && !($(event.target).hasClass('weeds'))) {
       $(event.target).addClass('plant');
       $(event.target).addClass(player[0]);
       rotatePlayer();
     }
+    removeWeeds(event);
   }
 
+  // generate the playing field
   const makeField = () => {
     for (let i = 0; i < 96; i++) {
       const $div = $('<div>');
@@ -25,13 +36,12 @@ $(() => {
       $container.append($div);
     }
   }
-
+  // randomly generate weeds on the field
   const generateWeeds = () => {
     for (let i = 0; i < 56; i++) {
       $container.children().eq(Math.floor(Math.random() * $container.children().length)).addClass('weeds')
     }
   }
-
 
   makeField();
   generateWeeds();
